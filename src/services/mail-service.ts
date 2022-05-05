2a7593aaa298f338fb6e141e190818b0013bad60
samnsimson <samnishanth01@gmail.com>
2022-05-05 20:04:58 +0530

    await for mail api keys

--- src/services/mail-service.ts
+++ src/services/mail-service.ts
@@ -1,21 +1,20 @@
 import Email from "@sendgrid/mail";
-import { SSM } from "aws-sdk";
 import { getFromSSM } from "../helpers/helpers";
 import { EmailBody } from "../types/types";
 
 const { EMAIL_API_KEY } = process.env;
 export default class EmailService {
-    protected ssm: SSM;
-    protected apikey: any;
-    constructor() {
-        this.ssm = new SSM({ region: "ap-south-1" });
-        this.apikey = getFromSSM(String(EMAIL_API_KEY)).then((key) => key);
-        this.apikey.then((key: string) => Email.setApiKey(key));
-    }
+    constructor() {}
+
+    private getKeys = async () => {
+        const key = await getFromSSM(String(EMAIL_API_KEY)).then((key) => key);
+        Email.setApiKey(String(key));
+    };
 
     public send = async (message: EmailBody) => {
         return new Promise(async (resolve, reject) => {
             try {
+                await this.getKeys();
                 await Email.send(message);
                 resolve({ sent: true, email: message.to });
             } catch (error) {
